b2913de7cd2ebac7816ed8c4092cf68cb84e514e
cjh <1418503647@qq.com>
2017-11-13 00:09:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# tool-front
+
+> 云设工具（[tool.yunser.com](http://tool.yunser.com/)）的开源版本。
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/router/index.js
@@ -0,0 +1,49 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+const Home = resolve => require(['@/components/Home'], resolve)
+const Main = resolve => require(['@/components/Main'], resolve)
+const About = resolve => require(['@/components/About'], resolve)
+const Count = resolve => require(['@/components/Count'], resolve)
+const Counter = resolve => require(['@/components/Counter'], resolve)
+const History = resolve => require(['@/components/History'], resolve)
+const More = resolve => require(['@/components/More'], resolve)
+const Add = resolve => require(['@/components/Add'], resolve)
+
+Vue.use(Router)
+
+let routes = [
+    {
+        path: '/',
+        component: Home,
+        children: [
+            {
+                path: '',
+                component: Main
+            }
+        ]
+    }, {
+        path: '/count',
+        component: Count
+    }, {
+        path: '/counter',
+        component: Counter
+    }, {
+        path: '/about',
+        component: About
+    }, {
+        path: '/history',
+        component: History
+    }, {
+        path: '/more',
+        component: More
+    }, {
+        path: '/add',
+        component: Add
+    }
+]
+
+export default new Router({
+    mode: 'history',
+    routes: routes
+})
